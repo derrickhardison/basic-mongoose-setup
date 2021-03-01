50b69f2d9bd221fcd9027b4d68906dfbc713571c
dlhardi <derricklhardison@gmail.com>
2021-03-01 10:34:51 -0500

    Created a mongoose relationship

--- app.js
+++ app.js
@@ -7,52 +7,63 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", {
 });
 
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  // DATA VALIDATION
+  name: { type: String, required: [true, "Please enter a name"] },
+  // DATA VALIDATION
+  rating: { type: Number, min: 1, max: 10 },
   review: String,
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-  name: "Apple",
-  rating: 7,
+  rating: 34,
   review: "Pretty solid as a fruit.",
 });
 
-// fruit.save();
+fruit.save();
 
 const peopleSchema = new mongoose.Schema({
   name: String,
   age: Number,
+  favoriteFruit: fruitSchema,
 });
 
 const Person = mongoose.model("Person", peopleSchema);
 
+const mango = new Fruit({
+  name: "Mango",
+  score: 10,
+  review: "Love this fruit",
+});
+
+mango.save();
+
 const person = new Person({
   name: "John",
   age: 37,
+  favoriteFruit: mango,
 });
 
-// person.save();
+ person.save();
 
-const kiwi = new Fruit({
-  name: "Kiwi",
-  score: 10,
-  review: "The best fruit!",
-});
+// const kiwi = new Fruit({
+//   name: "Kiwi",
+//   score: 10,
+//   review: "The best fruit!",
+// });
 
-const orange = new Fruit({
-  name: "Orange",
-  score: 9,
-  review: "Lovely fruit!",
-});
+// const orange = new Fruit({
+//   name: "Orange",
+//   score: 9,
+//   review: "Lovely fruit!",
+// });
 
-const banana = new Fruit({
-  name: "Banana",
-  score: 7,
-  review: "Okay fruit",
-});
+// const banana = new Fruit({
+//   name: "Banana",
+//   score: 7,
+//   review: "Okay fruit",
+// });
 
 ////
 // Inserts multiple documents to the fruits collection in the fruitsDB
@@ -66,13 +77,13 @@ const banana = new Fruit({
 //   }
 // });
 
-Fruit.find(function (err, fruits) {
-  if (err) {
-    console.log(err);
-  } else {
-  }
-  mongoose.connection.close();
-  fruits.map((fruit) => {
-    console.log(`The fruit name is: ${fruit.name}`);
-  });
-});
+// Fruit.find(function (err, fruits) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//   }
+//   mongoose.connection.close();
+//   fruits.map((fruit) => {
+//     console.log(`The fruit name is: ${fruit.name}`);
+//   });
+// });
